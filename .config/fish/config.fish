afc52895ba4d86e114502fd1fccad53679a0cfe3
Igor Wojciechowski <igorxwojciechowski@gmail.com>
2022-10-11 00:08:23 +0200

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,8 +16,8 @@ set color_prompt 'brblack'
 set fg_color 'cyan'
 
 # --- Tools
-alias ll='env EXA_COLORS="ur=0;32:uw=0;32:ux=0;32:ue=0;32:gr=0;32:gw=0;32:gx=0;32:tr=0;32:tw=0;32:tx=0;32:sn=1;36:uu=0;36:gu=0;36:gn=0;36" LS_COLORS="ex=1;36:di=0;36" exa -T -L 1 -l -g --icons -a'
-alias lt='env EXA_COLORS="ur=0;32:uw=0;32:ux=0;32:ue=0;32:gr=0;32:gw=0;32:gx=0;32:tr=0;32:tw=0;32:tx=0;32:sn=1;36:uu=0;36:gu=0;36:gn=0;36" LS_COLORS="ex=1;36:di=0;36" exa -T -L 2 -l -g --icons -a'
+alias ll='env EXA_COLORS="ur=0;32:uw=0;32:ux=0;32:ue=0;32:gr=0;32:gw=0;32:gx=0;32:tr=0;32:tw=0;32:tx=0;32:sn=1;36:uu=0;36:gu=0;36:gn=0;36:da=1;30" LS_COLORS="ex=1;32:di=0;32" exa -T -L 1 -l -g --icons -a'
+alias lt='env EXA_COLORS="ur=0;32:uw=0;32:ux=0;32:ue=0;32:gr=0;32:gw=0;32:gx=0;32:tr=0;32:tw=0;32:tx=0;32:sn=1;36:uu=0;36:gu=0;36:gn=0;36:da=1;320" LS_COLORS="ex=1;32:di=0;32" exa -T -L 2 -l -g --icons -a'
 
 # --- Applications
 alias venv="source ~/venvs/venv/bin/activate.fish"
