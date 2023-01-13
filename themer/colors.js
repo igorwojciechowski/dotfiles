c55258c70a7d7e98288ff4bf14221506ad73678c
Igor Wojciechowski <igorxwojciechowski@gmail.com>
2023-01-13 06:47:01 +0100

    add windows light theme

--- themer/colors.js
+++ themer/colors.js
@@ -1,39 +1,20 @@
 module.exports.colors = {
-
-    // A color set can have both light and dark variants, but is only required
-    // to have one.
-    dark: {
-  
-      // Colors can be defined in any valid CSS format.
-  
-      // accent0-7 should be the main accent colors of your theme. See the table
-      // in the "Color mappings" section for how the colors will be used in your
-      // new themes.
-      accent0: '#cc5a5a',
-      accent1: '#5acc97',
-      accent2: '#9bffd1',
-      accent3: '#57c46e',
-      accent4: '#5acc97', 
-      accent5: '#48768d',
-      accent6: '#1d4e66',
-      accent7: '#22698c',
-  
-      // shade0-7 should be shades of the same hue, with shade0 being the
-      // background and shade7 being the foreground. If you omit the
-      // intermediate shades (1 through 6), they will be calculated automatically
-      // for you.
-      shade0: '#113547',
-      shade1: '#294959',
-      shade2: '#415d6c',
-      shade3: '#58727e',
-      shade4: '#708691',
-      shade5: '#889aa3',
-      shade6: '#a0aeb5',
-      shade7: '#b8c2c8'
-  
-    },
-  
-    // Same as above, except that shade0 should be the lightest and shade7 should
-    // be the darkest.
-  
+    light: { 
+        accent0: '#A87777',
+        accent1: '#3e566a',
+        accent2: '#47627a',
+        accent3: '#506f89',
+        accent4: '#597b98',
+        accent5: '#6a88a2',
+        accent6: '#7a95ad',
+        accent7: '#8ba3b7',
+        shade0: '#c8cdd0',
+        shade1: '#bfc4c8',
+        shade2: '#b6bcc1',
+        shade3: '#adb3b9',
+        shade4: '#a4abb1',
+        shade5: '#949a9f',
+        shade6: '#83898e',
+        shade7: '#73787c'
+     }
   };
