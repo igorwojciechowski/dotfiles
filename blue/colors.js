98822efb7df14bd9872833c6d14663cd6e54a911
Igor Wojciechowski <igorxwojciechowski@gmail.com>
2023-02-11 20:46:54 +0100

    add blue theme

---
+++ blue/colors.js
@@ -0,0 +1,39 @@
+module.exports.colors = {
+
+    // A color set can have both light and dark variants, but is only required
+    // to have one.
+    dark: {
+  
+      // Colors can be defined in any valid CSS format.
+  
+      // accent0-7 should be the main accent colors of your theme. See the table
+      // in the "Color mappings" section for how the colors will be used in your
+      // new themes.
+      accent0: '#d95757',
+      accent1: '#317782',
+      accent2: '#8fbdc4',
+      accent3: '#38d9f2',
+      accent4: '#55c3d4',
+      accent5: '#55c3d4',
+      accent6: '#187887',
+      accent7: '#187887',
+  
+      // shade0-7 should be shades of the same hue, with shade0 being the
+      // background and shade7 being the foreground. If you omit the
+      // intermediate shades (1 through 6), they will be calculated automatically
+      // for you.
+      shade0: '#1f2e47',
+      shade1: '#354359',
+      shade2: '#4c586c',
+      shade3: '#626d7e',
+      shade4: '#798291',
+      shade5: '#8f97a3',
+      shade6: '#a5abb5',
+      shade7: '#bcc0c8'
+  
+    },
+  
+    // Same as above, except that shade0 should be the lightest and shade7 should
+    // be the darkest.
+  
+  };
