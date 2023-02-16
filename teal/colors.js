7a118680a66b190a67f56152b2df06b96b8bb023
Igor Wojciechowski <igorxwojciechowski@gmail.com>
2023-02-16 18:58:16 +0100

    add teal theme

---
+++ teal/colors.js
@@ -0,0 +1,39 @@
+module.exports.colors = {
+
+    // A color set can have both light and dark variants, but is only required
+    // to have one.
+    dark: {
+  
+      // Colors can be defined in any valid CSS format.
+  
+      // accent0-7 should be the main accent colors of your theme. See the table
+      // in the "Color mappings" section for how the colors will be used in your
+      // new themes.
+      accent0: '#ff4545',
+      accent1: '#0affb6',
+      accent2: '#91ffde',
+      accent3: '#00d998',
+      accent4: '#00d9bc',
+      accent5: '#42fce3',
+      accent6: '#1a8f6c',
+      accent7: '#439c81',
+  
+      // shade0-7 should be shades of the same hue, with shade0 being the
+      // background and shade7 being the foreground. If you omit the
+      // intermediate shades (1 through 6), they will be calculated automatically
+      // for you.
+      shade0: '#191919',
+      shade1: '#303030',
+      shade2: '#474747',
+      shade3: '#5e5e5e',
+      shade4: '#757575',
+      shade5: '#8c8c8c',
+      shade6: '#a3a3a3',
+      shade7: '#bababa'
+  
+    },
+  
+    // Same as above, except that shade0 should be the lightest and shade7 should
+    // be the darkest.
+  
+  };
