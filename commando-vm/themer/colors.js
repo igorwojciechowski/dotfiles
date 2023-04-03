b2e4f6c1d3f08ffdd6800fefc6199c3021355c69
Igor Wojciechowski <igorxwojciechowski@gmail.com>
2023-04-04 01:17:19 +0200

    add commando-vm theme

---
+++ commando-vm/themer/colors.js
@@ -0,0 +1,28 @@
+import type { ColorSet } from "themer";
+
+  const colors: ColorSet = {
+    name: 'Commando',
+    variants: {
+        dark: {
+            accent0: '#d41919',
+            accent1: '#5ebdac',
+            accent2: '#fea44c',
+            accent3: '#5ebdac',
+            accent4: '#49afe6',
+            accent5: '#367af0',
+            accent6: '#952ac3',
+            accent7: '#9755b3',
+            
+            shade0: '#23252e',
+            shade1: '#373a48',
+            shade2: '#4d5265',
+            shade3: '#636982',
+            shade4: '#7d829c',
+            shade5: '#9a9eb2',
+            shade6: '#b7bac8',
+            shade7: '#f1f1f4'
+        }
+    }
+  };
+
+  export default colors
---
+++ commando-vm/themer/themer-output/Commando/README.md
@@ -0,0 +1,11 @@
+# themer - Commando
+
+## VS Code
+
+Copy (or symlink) the generated package directory into the VS Code extensions directory:
+
+```
+cp -R 'VS Code/themer-commando' ~/.vscode/extensions/
+```
+
+Then reload or restart VS Code. The generated theme package should be in the list of installed extensions, and "Themer Commando Dark" will be available in the list of themes.
