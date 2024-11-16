8563f7bf61175520e0b4fcd73a94a431ea2d047a
Igor Wojciechowski <igorxwojciechowski@gmail.com>
2024-11-16 17:36:01 +0100

    update colors.js

--- colors.js
+++ colors.js
@@ -9,14 +9,14 @@ module.exports.colors = {
     // accent0-7 should be the main accent colors of your theme. See the table
     // in the "Color mappings" section for how the colors will be used in your
     // new themes.
-    accent0: '#d95757',
-    accent1: '#317782',
-    accent2: '#8fbdc4',
-    accent3: '#469F97',
+    accent0: '#e15b5b',
+    accent1: '#54C0B6',
+    accent2: '#6BCCDB',
+    accent3: '#53D0A6',
     accent4: '#54C0B6',
-    accent5: '#489AB1',
-    accent6: '#187887',
-    accent7: '#187887',
+    accent5: '#55ABC3',
+    accent6: '#35a9bb',
+    accent7: '#35a9bb',
 
     // shade0-7 should be shades of the same hue, with shade0 being the
     // background and shade7 being the foreground. If you omit the
